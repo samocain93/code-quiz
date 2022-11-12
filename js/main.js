3d952359ac9f2f7391123f86fb409ad6b3a20f38
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-11 23:38:30 -0600

    question setups and added answer feature

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const question = document.getElementById("question");
+const startButton = document.getElementById("start-btn");
+const highScores = document.getElementById("high-scores-btn");
+
+
+
+
+
+
+const questions = [
+    {
+        
+    }
+]
