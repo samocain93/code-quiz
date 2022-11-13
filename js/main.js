91350125de44a21d4d31e0aa024255ba2a5151ba
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-12 19:42:02 -0600

    got timer function working

--- js/main.js
+++ js/main.js
@@ -20,105 +20,83 @@ const questionEl = document.getElementById("question")
 const startButtonEl = document.getElementById("start-btn")
 const highScoresEl = document.getElementById("high-scores-btn")
 const cardEl = document.getElementById("question-container")
-const questionEl = document.getElementById("question")
 const answersEl = document.getElementById("answer-buttons")
+const resetButtonEl = document.getElementById("reset-game")
+const clearHighScores = document.getElementById("clear-btn")
+const scoresEl = document.getElementById("scores")
+const nextButtonEl = document.getElementById("next-btn")
+const currentScoreEl = document.getElementById("current-score")
+const remainingTimeEl = document.getElementById("timer")
 
 let score = 0;
 let questionCounter = 0
+var highScores = [];
+var interval;
+var timeRemaining = 60;
 
 
 
+// Sets timer to count down during quiz
 
-// Variable of questions array for quiz
-const questions = [
-    {
-        question: "Inside which element do we put the JavaScript file?",
-        choice1: ">Java>",
-        choice2: "<script>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 2
-    },
-    {
-        question: "What is the correct way to write 'Hello World' to the alert box?",
-        choice1: "alertBox('Hello World')",
-        choice2: "Window.message('Hello World')",
-        choice3: "userPrompt('Hello World')",
-        choice4: "alert('Hello World')",
-        answer: 4
-    },
-    {
-        question: "How do you create a function in JavaScript?",
-        choice1: "function newFunction()",
-        choice2: "function = newFunction()",
-        choice3: "function = newFunction()",
-        choice4: "createFunction = new()",
-        answer: 1
-    },
-    {
-        question: "How does a for loop start?",
-        choice1: "for (i = 0, i < 10, i++)",
-        choice2: "for (i < 5; i++)",
-        choice3: "for (i = 0; i < 10; i++)",
-        choice4: "while (i = 0; i < 10; i++)",
-        answer: 3
-    },
-    {
-        question: "How do you comment in JavaScript?",
-        choice1: "** This is a comment **",
-        choice2: "!-- This is a comment --!",
-        choice3: "<This is a comment>",
-        choice4: "// This is a comment",
-        answer: 4
-    },
+function startTimer() {
+    remainingTimeEl.textContent = timeRemaining;
+    interval = setInterval(function () {
+        timeRemaining--;
+        remainingTimeEl.textContent = timeRemaining;
 
-]
-
-
-
-
-
-
-
-startButton.addEventListener("click", startGame)
-
-
-
-function startGame() {
-
-    setNextQuestion()
-
+        if (timeRemaining === 0){
+            clearInterval(interval);
+            cardEl.textContent = "Time is up!"
+            cardEl.style.fontSize = "2rem"
+        }
+    }, 1000)
 }
+startTimer()
 
 
-function setNextQuestion() {
-    showQuestion(getRandomQuestion)
-}
 
 
-function showQuestion(question) {
-    cardQuestion.innerText = question.question
-}
 
 
-function selectAnswer() {
+// startButtonEl.addEventListener("click", startGame)
 
 
-}
 
-function getRandomQuestion() {
+// function startGame() {
 
-}
+//     setNextQuestion()
 
-
-function hide(element) {
-    element.style.display = "none";
-}
+// }
 
 
-function show(element) {
-    element.style.display = "block";
-}
+// function setNextQuestion() {
+//     showQuestion(getRandomQuestion)
+// }
+
+
+// function showQuestion(question) {
+//     cardQuestion.innerText = question.question
+// }
+
+
+// function selectAnswer() {
+
+
+// }
+
+// function getRandomQuestion() {
+
+// }
+
+
+// function hide(element) {
+//     element.style.display = "none";
+// }
+
+
+// function show(element) {
+//     element.style.display = "block";
+// }
 
 
 
---
+++ js/questions.js
@@ -0,0 +1,46 @@
+// Quiz Questions
+
+
+const questions = [
+    {
+        question: "Inside which element do we put the JavaScript file?",
+        choice1: ">Java>",
+        choice2: "<script>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer: 2
+    },
+    {
+        question: "What is the correct way to write 'Hello World' to the alert box?",
+        choice1: "alertBox('Hello World')",
+        choice2: "Window.message('Hello World')",
+        choice3: "userPrompt('Hello World')",
+        choice4: "alert('Hello World')",
+        answer: 4
+    },
+    {
+        question: "How do you create a function in JavaScript?",
+        choice1: "function newFunction()",
+        choice2: "function = newFunction()",
+        choice3: "function = newFunction()",
+        choice4: "createFunction = new()",
+        answer: 1
+    },
+    {
+        question: "How does a for loop start?",
+        choice1: "for (i = 0, i < 10, i++)",
+        choice2: "for (i < 5; i++)",
+        choice3: "for (i = 0; i < 10; i++)",
+        choice4: "while (i = 0; i < 10; i++)",
+        answer: 3
+    },
+    {
+        question: "How do you comment in JavaScript?",
+        choice1: "** This is a comment **",
+        choice2: "!-- This is a comment --!",
+        choice3: "<This is a comment>",
+        choice4: "// This is a comment",
+        answer: 4
+    },
+
+]
