cb259bd813b8dd1f99a27a676ffbf7357ce2bc67
Sam O'Cain <samocain@Sams-MBP.lan>
2022-11-16 21:43:59 -0600

    started scores js page

--- js/scores.js
+++ js/scores.js
@@ -1,6 +1,22 @@
+const initialsEl = document.getElementById("initials")
+const submitInitialsBtn = document.getElementById("high-scores-button")
+const resetBtn = document.getElementById("reset-game")
+const clearScoresBtn = document.getElementById("clear-btn")
+
+var highScoreArr = []
+
+
+
 var userFinalScore = window.location.search.slice(-2);
+highScoreArr.push(userFinalScore)
+console.log(highScoreArr)
+
+
 
 // console.log(userFinalScore)
 
 var displayFinalScore = document.getElementById("user-score")
 displayFinalScore.textContent = userFinalScore
+
+
+
